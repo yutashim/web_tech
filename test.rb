f2c96bf019c1830a05a6fdae310c89c3bce1b254
yuta <az7xj5xn@yahoo.co.jp>
2020-11-16 21:57:23 +0900

    first commit

---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
+server.mount('/kadai.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai.rb')
+server.start
